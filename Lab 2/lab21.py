e386ff08341c8957b2d6e4128902a8efca876faf
Nicholas <nicholas.garth1@gmail.com>
2021-09-14 13:39:25 -0400

    did it work

--- Lab 2/lab21.py
+++ Lab 2/lab21.py
@@ -1,7 +1,7 @@
 #Set celcius value as  a variable
-degrees_c = 20.0
+degrees_c = 20.0 
 
 #convert celcius value to fahrenheit and assign it to a new fahrenheit variable
 degrees_f = (degrees_c * 9/5) + 32
 
-print((degrees_f),'℉')
+print((degrees_f),'℉') 
